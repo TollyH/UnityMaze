9b111f3d11785544b30d77dcc62f13480b0ae745
Tolly <34184902+TollyH@users.noreply.github.com>
2023-03-22 23:40:23 +0000

    Implement JSON loading

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,3 +1,8 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
@@ -5,6 +10,8 @@ public class LevelManager : MonoBehaviour
     public static LevelManager Instance { get; private set; }
 
     public int CurrentLevelIndex { get; private set; }
+
+    public Level[] LoadedLevels { get; private set; }
 
     private readonly float unitSize = 1.8f;
 
@@ -15,6 +22,7 @@ public class LevelManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            LoadLevelJson(Path.Join(Application.streamingAssetsPath, "maze_levels.json"));
         }
         else
         {
@@ -30,5 +38,22 @@ public class LevelManager : MonoBehaviour
     public void LoadLevel(int levelIndex)
     {
         CurrentLevelIndex = levelIndex;
+    }
+
+    /// <summary>
+    /// Load and deserialize a level JSON file. The file must be a list of levels as created by the <see cref="SaveLevelJson"/> function.
+    /// </summary>
+    public void LoadLevelJson(string path)
+    {
+        List<JsonLevel> jsonLevels = JsonConvert.DeserializeObject<List<JsonLevel>>(File.ReadAllText(path));
+        LoadedLevels = jsonLevels is null ? Array.Empty<Level>() : jsonLevels.Select(x => x.GetLevel()).ToArray();
+    }
+
+    /// <summary>
+    /// Serialize and save the list of levels.
+    /// </summary>
+    public void SaveLevelJson(string path)
+    {
+        File.WriteAllText(path, JsonConvert.SerializeObject(LoadedLevels.Select(x => x.GetJsonLevel())));
     }
 }
