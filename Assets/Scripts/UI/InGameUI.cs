9c4ac79ecb5f6d6fd5a2ae21f17cb546d00f8c06
TollyH <ptolemyhill@sky.com>
2023-03-28 17:19:33 +0100

    Show UI in VR

--- Assets/Scripts/UI/InGameUI.cs
+++ Assets/Scripts/UI/InGameUI.cs
@@ -1,9 +1,13 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR;
 
 public class InGameUI : MonoBehaviour
 {
+    private Canvas thisCanvas;
+    private CanvasScaler thisScaler;
+
     [SerializeField]
     private Image statsPanel;
     [SerializeField]
@@ -13,8 +17,17 @@ public class InGameUI : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI timeLabel;
 
+    private void Awake()
+    {
+        thisCanvas = GetComponent<Canvas>();
+        thisScaler = GetComponent<CanvasScaler>();
+    }
+
     private void Update()
     {
+        thisCanvas.renderMode = XRSettings.enabled ? RenderMode.ScreenSpaceCamera : RenderMode.ScreenSpaceOverlay;
+        thisScaler.uiScaleMode = XRSettings.enabled ? CanvasScaler.ScaleMode.ScaleWithScreenSize : CanvasScaler.ScaleMode.ConstantPixelSize;
+
         KeysManager keys = LevelManager.Instance.KeysManager;
         PlayerManager player = LevelManager.Instance.PlayerManager;
         MonsterManager monster = LevelManager.Instance.MonsterManager;
