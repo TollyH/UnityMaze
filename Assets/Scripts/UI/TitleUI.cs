cf8c63773878d94d71033f2d2521b9c14b700d8a
TollyH <ptolemyhill@sky.com>
2023-04-28 23:49:41 +0100

    Add bindings for title screen button

--- Assets/Scripts/UI/TitleUI.cs
+++ Assets/Scripts/UI/TitleUI.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.XR;
 
@@ -7,15 +8,33 @@ public class TitleUI : MonoBehaviour
     private Canvas thisCanvas;
     private CanvasScaler thisScaler;
 
+    private TitleScreenControls inputActions;
+
     private void Awake()
     {
         thisCanvas = GetComponent<Canvas>();
         thisScaler = GetComponent<CanvasScaler>();
+        inputActions = new TitleScreenControls();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Buttons.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Buttons.Disable();
     }
 
     private void Update()
     {
         thisCanvas.renderMode = XRSettings.enabled ? RenderMode.WorldSpace : RenderMode.ScreenSpaceOverlay;
         thisScaler.uiScaleMode = XRSettings.enabled ? CanvasScaler.ScaleMode.ScaleWithScreenSize : CanvasScaler.ScaleMode.ConstantPixelSize;
+    }
+
+    public void OnPlay()
+    {
+        SceneManager.LoadScene("Scenes/MazeLevelScene");
     }
 }
